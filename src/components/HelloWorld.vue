97432052fb86dd8292e9d5154c81561673468061
unknown <wangjingpei@cp-wangjingpei.geostar.com.cn>
2019-06-01 14:04:26 +0800

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <ul>
+      <li>
+        <video-player-box url="http://vjs.zencdn.net/v/oceans.mp4"
+                          imgUrl="https://surmon-china.github.io/vue-quill-editor/static/images/surmon-1.jpg"/>
+      </li>
+      <br/>
+      <li>
+        <video-player-box url="https://f.us.sinaimg.cn/001DUIeGlx07tjpom09G010412037Spj0E020.mp4?label=mp4_720p&template=1280x720.23.0&Expires=1556097921&ssig=JqjKSmr3Oa&KID=unistore,video"
+                          imgUrl=""/>
+      </li>
+      <br/>
+      <li>
+        <video-player-box url="https://f.us.sinaimg.cn/000PuCXwlx07tktgw4Qo01041202eZPG0E010.mp4?label=mp4_720p&template=1280x720.23.0&Expires=1556096912&ssig=d9w56dqsit&KID=unistore,video"
+                          imgUrl=""/>
+      </li>
+      <br/>
+      <li>
+        <video-player-box url="https://f.us.sinaimg.cn/003qGFBclx07tjrG1o3u01041200rMBz0E010.mp4?label=mp4_hd&template=852x480.28.0&Expires=1556097776&ssig=sE64uJ0bFf&KID=unistore,video"
+                          imgUrl=""/>
+      </li>
+      <br/>
+    </ul>
+  </div>
+</template>
+
+<script>
+import VideoPlayerBox from '@/components/VideoPlayerBox'
+export default {
+  name: 'HelloWorld',
+  components: {
+    VideoPlayerBox
+  },
+  data () {
+    return {
+      msg: 'Welcome to My VideoPlayer'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,21 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+// 第一个是videoJs的样式，后一个是vue-video-player的样式，因为考虑到我其他业务组件可能也会用到视频播放，所以就放在了main.js内
+import 'video.js/dist/video-js.css'
+import 'vue-video-player/src/custom-theme.css'
+import VideoPlayer from 'vue-video-player'
+
+Vue.use(VideoPlayer)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
